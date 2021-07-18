a142efd37430760d720141c11d6d59a870622f33
myw823 <manyiuwai2014@gmail.com>
2021-07-18 17:29:29 +0800

    Finished Task5 - OffByN

---
+++ proj1a/TestOffByN.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestOffByN {
+    @Test
+    public void TestequalChars() {
+        OffByN offBy5 = new OffByN(5);
+        assertTrue(offBy5.equalChars('a', 'f')); //true
+        assertTrue(offBy5.equalChars('f', 'a'));  // true
+        assertFalse(offBy5.equalChars('f', 'h'));  // false
+    }
+}
